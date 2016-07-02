293160b7ee8aa6c3fe1972d8bc7526a476558014
thorlei <lzlgz2011@163.com>
2016-07-02 10:38:38 +0800

    添加服务提供者
    Signed-off-by: thorlei <lzlgz2011@163.com>

---
+++ src/Providers/ThorleiServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Thorlei\Laravel\Admin\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ThorleiServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
